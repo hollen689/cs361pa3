2ef85e2ffd0263d18abb7f1f1ebec1ce170d8180
Alex Hollenbeck <hollenal@dukes.jmu.edu>
2025-04-24 13:04:43 -0400

    Yaro's bad code

--- opt_proj/qserver.c
+++ opt_proj/qserver.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -13,160 +12,181 @@
 
 #define	QLEN			5
 #define	BUFSIZE			4096
+//Buffer size for names
+#define NBUF            64
+
+//Struct for each client storing their socket, name, and score
+typedef struct {
+    int socket;
+    char name[NBUF];
+    int score;
+} client_info_t;
 
 int group_size = 0;
 int client_count = 0;
-ques_t *qbuf[MAXQ]; 
+ques_t *qbuf[MAXQ];
 
-//barriers to synchronize threads
+//condition variables and mutexes
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-c = false;
+bool c = false;
 
-int passivesock( char *service, char *protocol, int qlen, int *rport );
+pthread_mutex_t winner_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *client( void *s )
+//winner variables
+char winner_name[NBUF] = "";
+bool winner_set = false;
+
+int passivesock(char *service, char *protocol, int qlen, int *rport);
+
+void *client(void *s)
 {
-	char buf[BUFSIZE];
-	int cc;
-	int ssock = (int)(intptr_t) s;
+	//List of client thread sockets
+    static client_info_t clients[QLEN];
+    static int answers_received = 0;
 
-	client_count++;
-	if (client_count == 1) {
-		write(ssock, WADMIN, strlen(WADMIN));
-	} else if (client_count > 1 && client_count <= group_size){
-		write(ssock, WJOIN, strlen(WJOIN));
-	} else {
-		write(ssock, FULL, strlen(FULL));
-		close(ssock);
-		pthread_exit(NULL);
-	}
+    char buf[BUFSIZE];
+    int cc;
+    int ssock = (int)(intptr_t)s;
 
-	if ((cc = read(ssock, buf, BUFSIZE)) <= 0) {
-		printf("The client has gone.\n");
-		close(ssock);
-		pthread_exit(NULL);
-	}
+    pthread_mutex_lock(&lock);
+	//Indexing client thread sockets
+    int client_index = client_count;
+    clients[client_index].socket = ssock;
+    client_count++;
 
-	printf("Client wants to join the group\n");
+    if (client_count == 1) {
+        write(ssock, WADMIN, strlen(WADMIN));
+    } else if (client_count > 1 && client_count <= group_size) {
+        write(ssock, WJOIN, strlen(WJOIN));
+    } else {
+        write(ssock, FULL, strlen(FULL));
+        close(ssock);
+        pthread_mutex_unlock(&lock);
+        pthread_exit(NULL);
+    }
+    pthread_mutex_unlock(&lock);
 
-	if (strncmp(buf, GROUP, strlen(GROUP)) == 0) {
-		char *token = strtok(buf, "|"); // "GROUP"
-		char *name = strtok(NULL, "|"); // "Alice"
-		char *sizeStr = strtok(NULL, "\r\n"); // "4"
-		if (group_size == 0 && sizeStr != NULL) {
-			group_size = strtol(sizeStr, NULL, 10);
-			printf("Group size set to %d by leader %s\n", group_size, name);
-		}
-		write(ssock, WAIT, strlen(WAIT));
-	} else if (strncmp(buf, JOIN, strlen(JOIN)) == 0) {
-		char *token = strtok(buf, "|");
-		char *name = strtok(NULL, "|");
-		write(ssock, WAIT, strlen(WAIT));
-	}
+    if ((cc = read(ssock, buf, BUFSIZE)) <= 0) {
+        printf("The client has gone.\n");
+        close(ssock);
+        pthread_exit(NULL);
+    }
 
-	// Wait for all clients to join the group
-	pthread_mutex_lock(&lock);
-	if (client_count == group_size) {
-		c = true;
-		pthread_cond_broadcast(&cond);
-	} else {
-		while (!c) {
-			pthread_cond_wait(&cond, &lock);
-		}
-	}
-	pthread_mutex_unlock(&lock);
+    if (strncmp(buf, GROUP, strlen(GROUP)) == 0) {
+        char *token = strtok(buf, "|");
+        char *name = strtok(NULL, "|");
+        char *sizeStr = strtok(NULL, "\r\n");
+        if (group_size == 0 && sizeStr != NULL) {
+            group_size = strtol(sizeStr, NULL, 10);
+            printf("Group size set to %d by leader %s\n", group_size, name);
+        }
+        strncpy(clients[client_index].name, name, NBUF);
+        clients[client_index].score = 0;
+        write(ssock, WAIT, strlen(WAIT));
+    } else if (strncmp(buf, JOIN, strlen(JOIN)) == 0) {
+        char *token = strtok(buf, "|");
+        char *name = strtok(NULL, "|");
+        strncpy(clients[client_index].name, name, NBUF);
+        clients[client_index].score = 0;
+        write(ssock, WAIT, strlen(WAIT));
+    }
 
-	// Send the question to the client (I think this is how the conditions work)
-	char question_msg[BUFSIZE];
-	snprintf(question_msg, BUFSIZE, "QUES|%ld|%s", strlen(qbuf[0]->qtext), qbuf[0]->qtext);
-	write(ssock, question_msg, strlen(question_msg));
+    pthread_mutex_lock(&lock);
+    if (client_count == group_size) {
+        c = true;
+        pthread_cond_broadcast(&cond);
+    } else {
+        while (!c) {
+            pthread_cond_wait(&cond, &lock);
+        }
+    }
+    pthread_mutex_unlock(&lock);
 
-	pthread_exit(NULL);
+    // Send question
+    char question_msg[BUFSIZE];
+    snprintf(question_msg, BUFSIZE, "QUES|%ld|%s", strlen(qbuf[0]->qtext), qbuf[0]->qtext);
+    write(ssock, question_msg, strlen(question_msg));
+
+    // Wait for answer from client
+    if ((cc = read(ssock, buf, BUFSIZE)) <= 0) {
+        printf("The client has gone.\n");
+        close(ssock);
+        pthread_exit(NULL);
+    }
+
+    //Find winner
+    pthread_mutex_lock(&winner_mutex);
+    if (!winner_set) {
+        strncpy(winner_name, clients[client_index].name, NBUF);
+        winner_set = true;
+    }
+    pthread_mutex_unlock(&winner_mutex);
+
+    pthread_mutex_lock(&lock);
+    answers_received++;
+    if (answers_received == group_size) {
+        pthread_cond_broadcast(&cond);
+    } else {
+        while (answers_received < group_size) {
+            pthread_cond_wait(&cond, &lock);
+        }
+    }
+    pthread_mutex_unlock(&lock);
+
+    //Send winner announcement
+    char win_msg[BUFSIZE];
+    snprintf(win_msg, BUFSIZE, "WIN|%s\r\n", winner_name);
+    write(ssock, win_msg, strlen(win_msg));
+
+    pthread_exit(NULL);
 }
 
-
-/*
-*/
-int
-main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-	//buffer for commandline
-	//ques_t qbuf[BBUF];
-	//default stuff
-	char			*service;
-	struct sockaddr_in	fsin;
-	int			alen = sizeof(fsin);
-	int			msock;
-	int			ssock;
-	int			rport = 0;
-	//Counter for number of connections
-	//int client_count = 0;
-	
-	switch (argc) 
-	{
-		case	1:
-			// No args? let the OS choose a port and tell the user
-			rport = 1;
-			break;
-		case	2:
-			// User provides a port? then use it
-			service = argv[1];
-			break;
-		case	3:
-			read_questions(argv[1], qbuf);
-			service = argv[2];
-			break;
-		default:
-			fprintf( stderr, "usage: server [port]\n" );
-			exit(-1);
-	}
+    char *service;
+    struct sockaddr_in fsin;
+    int alen = sizeof(fsin);
+    int msock;
+    int ssock;
+    int rport = 0;
 
-	// Call the function that sets everything up
-	msock = passivesock( service, "tcp", QLEN, &rport );
+    switch (argc) {
+        case 1:
+            rport = 1;
+            break;
+        case 2:
+            service = argv[1];
+            break;
+        case 3:
+            read_questions(argv[1], qbuf);
+            service = argv[2];
+            break;
+        default:
+            fprintf(stderr, "usage: server [port]\n");
+            exit(-1);
+    }
 
-	if (rport)
-	{
-		// Tell the user the selected port number
-		printf( "server: port %d\n", rport );	
-		fflush( stdout );
-	}
+    msock = passivesock(service, "tcp", QLEN, &rport);
+    if (rport) {
+        printf("server: port %d\n", rport);	
+        fflush(stdout);
+    }
 
-	
-	for (;;)
-	{
-		int	ssock = accept( msock, (struct sockaddr *)&fsin, &alen );
-		pthread_t	thr;
+    for (;;) {
+        int ssock = accept(msock, (struct sockaddr *)&fsin, &alen);
+        pthread_t thr;
 
-		/*
-		if ((ssock = accept( msock, (struct sockaddr *)&fsin, &alen )) >= 0){
-			client_count++;
-			if (client_count == 1) {
-				// First client
-				write(ssock, WADMIN, strlen(WADMIN));
-			} else if (client_count > 1 && client_count <= group_size){
-				write(ssock, WJOIN, strlen(WJOIN));
-			} else {
-				write(ssock, FULL, strlen(FULL));
-				close(ssock);
-				continue;
-			}
-		}
-		*/
+        if (ssock < 0) {
+            fprintf(stderr, "accept: %s\n", strerror(errno));
+            break;
+        }
 
-		if (ssock < 0)
-		{
-			fprintf( stderr, "accept: %s\n", strerror(errno) );
-			break;
-		}
+        printf("A client has arrived for echoes - serving on fd %d.\n", ssock);
+        fflush(stdout);
 
-		printf( "A client has arrived for echoes - serving on fd %d.\n", ssock );
-		fflush( stdout );
+        pthread_create(&thr, NULL, client, (void *)(intptr_t)ssock);
+    }
 
-		pthread_create( &thr, NULL, client, (void *) ssock );
-
-	}
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
-
-
